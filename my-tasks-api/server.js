f0202784b6f9b9161ccc9e56c97a61107c10e101
Daniel Manor <danielmanor.lab@gmail.com>
2021-08-30 19:09:50 +0300

    backend - added endpoints for creating, pulling and deleting tasks

--- my-tasks-api/server.js
+++ my-tasks-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const Task = require("./models/task");
@@ -7,10 +8,97 @@ const Task = require("./models/task");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/tasks", async (req, res) => {
+  console.log("TRYING TO FETCH TASKS");
+
+  try {
+    const tasks = await Task.find();
+    res.status(200).json({
+      tasks: tasks.map((task) => ({
+        id: task.id,
+        text: task.text,
+      })),
+    });
+    console.log("FETCHED TASKS");
+  } catch (err) {
+    console.error("ERROR FETCHING TASKS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to load tasks." });
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  console.log("TRYING TO STORE TASK");
+  const taskText = req.body.text;
+
+  if (!taskText || taskText.trim().length === 0) {
+    console.log("INVALID INPUT - NO TEXT");
+    return res.status(422).json({ message: "Invalid goal text." });
+  }
+
+  const task = new Task({
+    text: taskText,
+  });
+
+  try {
+    await task.save();
+    res
+      .status(201)
+      .json({ message: "Task saved", task: { id: task.id, text: taskText } });
+    console.log("STORED NEW TASK");
+  } catch (err) {
+    console.error("ERROR FETCHING TASKS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to save task." });
+  }
+});
+
+app.get("/tasks", async (req, res) => {
+  console.log("TRYING TO FETCH TASKS");
+
+  try {
+    const tasks = await Task.find();
+    res.status(200).json({
+      tasks: tasks.map((task) => ({
+        id: task.id,
+        text: task.text,
+      })),
+    });
+    console.log("FETCHED TASKS");
+  } catch (err) {
+    console.error("ERROR FETCHING TASKS");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to load tasks." });
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  console.log("TRYING TO DELETE TASK");
+  const taskId = req.params.id;
+
+  try {
+    await Task.deleteOne({ _id: taskId });
+    res.status(200).json({ message: "Task deleted" });
+    console.log("DELETED TASK");
+  } catch (err) {
+    console.error("ERROR DELETING TASK");
+    console.error(err.message);
+    res.status(500).json({ message: "Failed to delete task." });
+  }
 });
 
 mongoose.connect(
