ce103ad9d626ea56b9137f2b787a8b4b86b46335
Daniel Manor <danielmanor.lab@gmail.com>
2021-09-01 11:12:24 +0300

    added react testing library and example test

--- my-tasks-client/src/App.js
+++ my-tasks-client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div role="main" className="App">
+      Hello World!
+    </div>
+  );
 }
 
 export default App;
